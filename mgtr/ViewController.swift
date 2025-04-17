5d72dcb4fc7cfa0e689badf02e2600d932d3e32d
zafarali56 <zafaralyjamali722@gmail.com>
2025-04-17 23:46:20 +0500

    tableViewController/TableView

--- mgtr/ViewController.swift
+++ mgtr/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var countriesTableView: UITableView!
+    struct Constants {
+        static let cellIdentifier = "MagicCountryCell"
+    }
+    var countries = ["Chine","Brazil","Australia","Canada","USA", "Pakistan", "Germany"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countriesTableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return countries.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for:indexPath)
+        var tableViewCellConfi = tableViewCell.defaultContentConfiguration()
+        tableViewCellConfi.text = countries[indexPath.row]
+        tableViewCell.contentConfiguration = tableViewCellConfi
+        return tableViewCell
+    }
+    
+    
+}
