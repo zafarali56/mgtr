64f59d68a07b455bc64ed6de69a8270d72ce4183
zafarali56 <zafaralyjamali722@gmail.com>
2025-04-17 21:35:38 +0500

    Initial Commit

---
+++ mgtr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mgtr
+//
+//  Created by Zafar ‎ on 17/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
