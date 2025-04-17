18bfbe400bc1747c30202b8aa4843ffb299e5cc8
zafarali56 <zafaralyjamali722@gmail.com>
2025-04-18 00:13:01 +0500

    sectionView list with mainText and secondText using maps and Arrays

--- mgtr/ViewController.swift
+++ mgtr/ViewController.swift
@@ -12,12 +12,15 @@ class ViewController: UIViewController {
     struct Constants {
         static let cellIdentifier = "MagicCountryCell"
     }
-    var countries = ["Chine","Brazil","Australia","Canada","USA", "Pakistan", "Germany"
+    var countries = ["China","Brazil","Australia","Canada","USA", "Pakistan", "Germany"
     ]
+    var continentsCountryMapping : Dictionary<String, String> = ["China":"Asia","Brazil":"South America","Australia":"Australia","Canada":"North America","USA":"North America", "Pakistan":"Asia", "Germany":"Germany"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         countriesTableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
+        countriesTableView.dataSource = self
+        countriesTableView.delegate = self
     }
 
 
@@ -34,10 +37,20 @@ extension ViewController: UITableViewDataSource{
 
         let tableViewCell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for:indexPath)
         var tableViewCellConfi = tableViewCell.defaultContentConfiguration()
-        tableViewCellConfi.text = countries[indexPath.row]
+        var countryNames = countries[indexPath.row]
+        tableViewCellConfi.text = countryNames
+        tableViewCellConfi.secondaryText = continentsCountryMapping[countryNames] ?? ""
         tableViewCell.contentConfiguration = tableViewCellConfi
         return tableViewCell
     }
     
     
+}
+
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("User tapped on cell at \(indexPath) index path")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
